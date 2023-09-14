b18879b6d44bc95abde940a8bc14d725cb08c455
Scott Moore <me@scottwillmoore.au>
2023-09-14 16:34:36 +1000

    Setup development environment with Nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nix-packages.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nix-systems.url = "github:nix-systems/default";
+  };
+  outputs = inputs @ {
+    flake-parts,
+    nix-packages,
+    nix-systems,
+    ...
+  }: let
+    packagesModule = {
+      perSystem = {system, ...}: {
+        _module.args.packages = import nix-packages {
+          inherit system;
+        };
+      };
+    };
+
+    systemsModule = {
+      systems = import nix-systems;
+    };
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        packagesModule
+        systemsModule
+      ];
+
+      perSystem = {packages, ...}: {
+        devShells.default = with packages;
+          mkShell {
+            nativeBuildInputs = [
+              nodejs
+            ];
+          };
+      };
+    };
+}
