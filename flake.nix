38f6fd0ebe9e7c303d42fe4ce01929e3d8a89ae4
Scott Moore <me@scottwillmoore.au>
2023-09-30 12:40:25 +1000

    Update Nix flake to use nix-foundations

--- flake.nix
+++ flake.nix
@@ -1,37 +1,13 @@
 {
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nix-packages.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nix-systems.url = "github:nix-systems/default";
+    nix-foundations.url = "github:scottwillmoore/nix-foundations";
   };
-  outputs = inputs @ {
-    flake-parts,
-    nix-packages,
-    nix-systems,
-    ...
-  }: let
-    packagesModule = {
-      perSystem = {system, ...}: {
-        _module.args.packages = import nix-packages {
-          inherit system;
-        };
-      };
-    };
-
-    systemsModule = {
-      systems = import nix-systems;
-    };
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        packagesModule
-        systemsModule
-      ];
-
+  outputs = inputs @ {nix-foundations, ...}:
+    nix-foundations.mkFlake inputs {
       perSystem = {packages, ...}: {
         devShells.default = with packages;
-          mkShell {
-            nativeBuildInputs = [
+          mkShellNoCC {
+            packages = [
               nodejs
               vsce
             ];
